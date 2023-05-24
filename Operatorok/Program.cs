c2ecd4a5b0eb533b40ba580acb60bc384aa232f6
Papp László Levente <papplaszlolevente@gmail.com>
2023-05-24 10:10:00 +0200

    Beolvasás kész

--- Operatorok/Program.cs
+++ Operatorok/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Operatorok;
+
+internal class Program
+{
+    static List<Kifejezesek> lista = new List<Kifejezesek>();
+    static void Main(string[] args)
+    {
+        StreamReader sr = new StreamReader("adatok\\kifejezesek.txt");
+        sr.ReadLine();
+        while (!sr.EndOfStream)
+        {
+            var mezok = sr.ReadLine().Split(';');
+
+            Kifejezesek uj = new Kifejezesek(int.Parse(mezok[0]), mezok[1] ,int.Parse(mezok[2]));
+        }
+        sr.Close();
+    }
+}
