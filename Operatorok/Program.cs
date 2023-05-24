b7c32075ee463c844093324070fcba489a405573
Papp László Levente <papplaszlolevente@gmail.com>
2023-05-24 11:47:56 +0200

    Óra végei commit

--- Operatorok/Program.cs
+++ Operatorok/Program.cs
@@ -27,8 +27,28 @@ internal class Program
         Console.WriteLine($"3.feladat: Kifejezések maradékos osztással: {lista.Where(x=>x.OPerator=="mod").Count()}");
 
         //4.
-       bool van= lista.Any(x => x.ElsoOperandus % 10 == 0 && x.MasodikOperandus % 10 == 0);
+        bool van= lista.Any(x => x.ElsoOperandus % 10 == 0 && x.MasodikOperandus % 10 == 0);
         Console.WriteLine($"{ (van==true ? "Van ilyen kifejezés" : "Nincs ilyen kifejezés")}");
 
+        //5.
+        Console.WriteLine("5.feladat: Statisztika:");
+        Console.WriteLine($"\tmod -> {lista.Count(a => a.OPerator == "mod")} db");
+        Console.WriteLine($"\t  / -> {lista.Count(a => a.OPerator == "/")} db");
+        Console.WriteLine($"\tdiv -> {lista.Count(a => a.OPerator == "div")} db");
+        Console.WriteLine($"\t  - -> {lista.Count(a => a.OPerator == "-")} db");
+        Console.WriteLine($"\t  * -> {lista.Count(a => a.OPerator == "*")} db");
+        Console.WriteLine($"\t  + -> {lista.Count(a => a.OPerator == "+")} db");
+
+        //6.
+
+        //7.
+
+        //8.
+        using (StreamWriter fajl = new StreamWriter("eredmenyek.txt"))
+        {
+            lista.ForEach(x => fajl.WriteLine(x.ToString()));
+        }
+        Console.WriteLine("8. feladat: eredmenyek.txt");
+        Console.ReadKey();
     }
 }
